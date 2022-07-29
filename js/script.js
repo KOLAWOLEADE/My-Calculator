09db34dd33a5030387861d749e15dacdad46fbcc
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-07-29 03:48:25 +0100

    add equal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-// $(document).ready
-(function()
- {$(".calc-btn").on("click",function(){
-    $(".result").val ($(".resuit").val() + $(this).html() );
+function insrt(num){
+    $(".result").val($(".result").val()+num);
+}
 
-    })
-
-    // $("")
-
+function equal(){
+    $(".result").val(eval($(".result").val()));
+}
 
 
+function c(){
+    $(".result").val("");
+}
 
-})
+
+
+
+
+
+
+
+
+
+
+//  $(document).ready (function insrt(num){
+// //  {$(".result")(function insert(num )
+//     $(".result").val ($(".resuit").val() + num );
+
+//     })
+
+//     // $(".delete").click(function(){
+//     //     $(".result").val("")
+//     // })
+    
+
+//     // $(".designs").click(function(){
+//     //     $(".result").val(eval($(".result").val()))
+//     // })
+
+
+
+
+// // })
